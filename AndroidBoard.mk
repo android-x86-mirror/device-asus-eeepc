29c072cfe99fab36a8a602b62ac8bf816f4f42ab
Chih-Wei Huang <cwhuang@linux.org.tw>
2009-12-10 16:19:13 +0800

    let eeepc inherit from generic_x86 target

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,47 +1,7 @@
 LOCAL_PATH := $(call my-dir)
+LOCAL_FIRMWARES := RT2860STA/RT2860STA.dat
+BOARD_WPA_SUPPLICANT_DRIVER := true
+TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/eeepc_info
+TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 
-####################################################################
-
-# When adding GL support, will have to specify this?
-#-include vendor/intel/g945/Android.mk
-
-# include more board specific stuff here? Audio params?! <shrug>
-
-
-####################################################################
-### Include input devices specific files
-
-define add-prebuilt-target
-
-file := $(1)/$(2)
-$$(file): $$(LOCAL_PATH)/$(2) | $$(ACP)
-	$$(transform-prebuilt-to-target)
-ALL_PREBUILT += $$(file)
-
-endef
-
-define add-keymap
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := $(1)
-include $(BUILD_KEY_CHAR_MAP)
-
-$$(eval $$(call add-prebuilt-target,$(TARGET_OUT_KEYLAYOUT),$$(subst .kcm,.kl,$(1))))
-
-endef
-
-$(eval $(call add-prebuilt-target,$(TARGET_ROOT_OUT),init.rc))
-$(INSTALLED_RAMDISK_TARGET): $(file)
-$(eval $(call add-prebuilt-target,$(TARGET_ROOT_OUT),init.eeepc.rc))
-$(INSTALLED_RAMDISK_TARGET): $(file)
-$(eval $(call add-prebuilt-target,$(TARGET_OUT_DATA_ETC),init.eeepc.sh))
-
-ALL_FIRMWARES := $(shell cd $(LOCAL_PATH) && find firmware -type f)
-$(eval $(foreach f,$(ALL_FIRMWARES),$(call add-prebuilt-target,$(TARGET_OUT_SHARED_LIBRARIES),$(f))))
-
-ALL_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
-$(eval $(foreach f,$(ALL_APPS),$(call add-prebuilt-target,$(TARGET_OUT),$(f))))
-
-LOCAL_PATH := $(LOCAL_PATH)/keymaps
-ALL_KEYMAPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/*.kcm))
-$(eval $(foreach f,$(ALL_KEYMAPS),$(call add-keymap,$(f))))
+include $(GENERIC_X86_ANDROID_MK)
--- eeepc.mk
+++ eeepc.mk
@@ -1,30 +1,6 @@
-# TODO: Need to setup a proper inheritance tree for this class of
-# products...
-#
-# For now, reuse the generic (phone) infrastructure.
-#
+PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
 
-PRODUCT_PACKAGES := \
-	ConnectBot \
-	Email \
-	FileManager \
-	GlobalTime \
-	JETBoy \
-	LunarLander \
-	NotePad \
-	PinyinIME \
-	RSSReader \
-	Snake
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+$(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
 
 PRODUCT_NAME := eeepc
 PRODUCT_DEVICE := eeepc
-PRODUCT_POLICY := android.policy_mid
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.com.android.dataroaming=true
-
-ifeq ($(TARGET_PRODUCT),$(PRODUCT_NAME))
-TARGET_ARCH := x86
-DISABLE_DEXPREOPT := true
-endif
